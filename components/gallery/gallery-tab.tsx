8a14f181ba52aff8f3839d7912ba78a4db06b4be
Oleksii Kotvytskyi <kot.aleksei43@gmail.com>
2023-09-10 19:48:32 +0300

    fix: fire deploy

--- components/gallery/gallery-tab.tsx
+++ components/gallery/gallery-tab.tsx
@@ -6,6 +6,7 @@ import { Image as ImageType } from "@/types";
 
 interface GalleryTabProps {
   image: ImageType;
+  children?: React.ReactNode;
 }
 
 const GalleryTab = ({ image }: GalleryTabProps) => {
